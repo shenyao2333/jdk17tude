65f55dcbcd06648c874bff5f3463d52ffe6583b6
sy <sy91aa@163.com>
2023-08-29 16:08:09 +0800

    项目初始化

---
+++ src/main/java/com/sy/jdk17tude/Jdk17tudeApplication.java
@@ -0,0 +1,13 @@
+package com.sy.jdk17tude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk17tudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk17tudeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sy/jdk17tude/Jdk17tudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.jdk17tude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk17tudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
